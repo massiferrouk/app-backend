6db09120b6555a37ff898bd78cf147d80c30b61e
massiferrouk <massiferrouk3@gmail.com>
2026-05-10 00:30:20 +0200

    feat(APP-47): add custom exception classes for business errors

---
+++ src/main/java/com/yuniv/backend/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.yuniv.backend.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
